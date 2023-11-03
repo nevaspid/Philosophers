131d40d0759b47dc701cf914eb43b45ec4017bae
gloms <109989668+nevaspid@users.noreply.github.com>
2023-11-03 21:59:45 +0100

    commit

--- srcs/time/health_handler.c
+++ srcs/time/health_handler.c
@@ -6,7 +6,7 @@
 /*   By: gloms <rbrendle@student.42mulhouse.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 19:22:01 by gloms             #+#    #+#             */
-/*   Updated: 2023/11/03 17:46:55 by gloms            ###   ########.fr       */
+/*   Updated: 2023/11/03 21:52:44 by gloms            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 int	death_check(t_philo *philo, t_gestion *monitor)
 {
+	//pthread_mutex_t	time_mutex;
+
+	//pthread_mutex_lock(&time_mutex);
 	monitor->start_time = get_time();
+	//pthread_mutex_unlock(&time_mutex);
 	while (philo->amidead == 0)
 		philo = philo->next;
 	return (1);
